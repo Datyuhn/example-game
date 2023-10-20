6239bc76327ae6c44cf6c51abdc970fe18154cd0
datyuh <datjenoo2511@gmail.com>
2023-10-20 18:13:45 +0700

    Create objects and mechanics
    + Bird
    + Pipes
    + Mechanic of bird and pipe spawning

---
+++ My project/Assets/Spawn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject pipe;
+    public float maxTime, height;
+    float timer = 0;
+
+    void Start()
+    {
+        timer = maxTime;
+    }
+    void Update()
+    {
+        if (timer > maxTime)
+        {
+            GameObject tmp = Instantiate(pipe, new Vector3(transform.position.x, transform.position.y + Random.Range(-height, height), 0), Quaternion.identity);
+            Destroy(tmp, 10f);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
