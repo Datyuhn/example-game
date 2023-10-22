c13273bb2ae1e719d43cd7de60265f315f95983b
MBBK5Ngoc <40780547+MBBK5Ngoc@users.noreply.github.com>
2023-10-22 11:20:04 +0700

    change behaviour of the bird

--- My project/Assets/Mechanics.cs
+++ My project/Assets/Mechanics.cs
@@ -4,58 +4,82 @@ using UnityEngine.SceneManagement;
 
 public class Mechanics : MonoBehaviour
 {
-    Rigidbody2D rg;
+    Rigidbody2D rb;
     public GameObject gameOverObject;
     public GameObject timerObject;
     public GameObject scoreObject;
     public GameObject startObject;
 
     public float speed;
+    public float rotationSpeed;
     public static Boolean ready, isDead;
 
-    void Start()
+    private void Start()
     {
         Time.timeScale = 1.0f;
         ready = false;
         isDead = false;
-        rg = GetComponent<Rigidbody2D>();
-        rg.gravityScale = -0.001f;
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
         gameOverObject.SetActive(false);
         timerObject.SetActive(false);
         scoreObject.SetActive(false);
     }
 
-    void GameOver()
+    private void Update()
+    {
+        if (!ready)
+        {
+            rb.gravityScale = 0;
+        }
+
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!ready)
+            {
+                StartGame();
+            }
+            Flap();
+        }
+        
+        transform.rotation = Quaternion.Euler(0,0,rb.velocity.y*rotationSpeed);
+    }
+    
+    private void StartGame()
+    {
+        rb.gravityScale = 1f;
+        Flap();
+        ready = true;
+        timerObject.SetActive(true);
+        scoreObject.SetActive(true);
+        startObject.SetActive(false);
+        if (!isDead)
+        {
+            FindObjectOfType<AudioManager>().Play("wing sound");
+        }
+    }
+
+    private void Flap()
+    {
+        rb.velocity = Vector2.up * speed;
+    }
+
+    private void GameOver()
     {
         Time.timeScale = 0;
         gameOverObject.SetActive(true);
         isDead = true;
         FindObjectOfType<AudioManager>().Play("dead");
     }
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(0);
-    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameOver();
     }
-    void Update()
-    {
-        if (!ready) 
-        {
-            rg.gravityScale = -0.001f;
-        }
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
-        {
-            rg.gravityScale = 1;
-            rg.AddForce(Vector2.up * speed);
-            ready = true;
-            timerObject.SetActive(true);
-            scoreObject.SetActive(true);
-            startObject.SetActive(false);
 
-            if (!isDead) FindObjectOfType<AudioManager>().Play("wing sound");
-        }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+    
 }
